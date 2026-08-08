1774a8cab0ad35fa074d1bdc3cdab24a9b0092cd
liudejin <liudejin@kis2.cn>
2026-08-08 09:40:03 +0800

    1.货主基础功能

---
+++ jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/service/IWmsCargoOwnersService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.wms.goods.service;
+
+import org.jeecg.modules.wms.goods.entity.WmsCargoOwners;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 货主表
+ * @Author: jeecg-boot
+ * @Date:   2026-06-27
+ * @Version: V1.0
+ */
+public interface IWmsCargoOwnersService extends IService<WmsCargoOwners> {
+
+}
---
+++ jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/service/impl/WmsCargoOwnersServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.wms.goods.service.impl;
+
+import org.jeecg.modules.wms.goods.entity.WmsCargoOwners;
+import org.jeecg.modules.wms.goods.mapper.WmsCargoOwnersMapper;
+import org.jeecg.modules.wms.goods.service.IWmsCargoOwnersService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 货主表
+ * @Author: jeecg-boot
+ * @Date:   2026-06-27
+ * @Version: V1.0
+ */
+@Service
+public class WmsCargoOwnersServiceImpl extends ServiceImpl<WmsCargoOwnersMapper, WmsCargoOwners> implements IWmsCargoOwnersService {
+
+}
